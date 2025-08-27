3da18743c9b82aed5a03b47318bc16bd1bd8474c
Lakshya <lakshyadhaka.987s@gmail.com>
2025-08-27 18:51:47 +0530

    backend connection

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT = `${import.meta.VITE_BACKEND_URL}/user`;
-export const JOB_API_END_POINT = `${import.meta.VITE_BACKEND_URL}/job`;
-export const APPLICATION_API_END_POINT = `${import.meta.VITE_BACKEND_URL}/application`;
-export const COMPANY_API_END_POINT = `${import.meta.VITE_BACKEND_URL}/company`;
+const BACKEND_URL = "https://prohire-jvh3.onrender.com"
+export const USER_API_END_POINT = `${BACKEND_URL}/user`;
+export const JOB_API_END_POINT = `${BACKEND_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BACKEND_URL}/application`;
+export const COMPANY_API_END_POINT = `${BACKEND_URL}/company`;
