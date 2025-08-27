6a0f241ad92f30a9d5a10117aa0042a559a21f94
Lakshya <lakshyadhaka.987s@gmail.com>
2025-08-27 17:23:32 +0530

    added env in frontend for backend api

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,4 @@
-const BASE_URL = 'http://localhost:8000';
-export const USER_API_END_POINT = `${BASE_URL}/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const USER_API_END_POINT = `${process.env.BASE_URL}/user`;
+export const JOB_API_END_POINT = `${process.env.BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${process.env.BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${process.env.BASE_URL}/company`;
